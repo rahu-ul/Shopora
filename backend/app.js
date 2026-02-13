85d85547931c213af35908da25ff061ad3d04507
rahu-ul <rahulsharmajan20@gmail.com>
2026-02-13 10:36:28 +0530

    app.js change

--- backend/app.js
+++ backend/app.js
@@ -34,6 +34,7 @@ app.use(cors(corsOptions));
 // app.options('*', cors(corsOptions));
 
 app.use(cookieParser());
+app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload({
